2c2e719a63f2d1ce33062c88c5593e3aecd2bc24
Gabriel Rascovit <gabrielrascovit@inf.ufg.br>
2016-11-07 00:17:19 -0200

    change in app colors; secondary buttons at the bottom of the screen

--- app/src/main/java/br/com/codinglab/achei/fragments/LoginFragment.java
+++ app/src/main/java/br/com/codinglab/achei/fragments/LoginFragment.java
@@ -5,10 +5,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import br.com.codinglab.achei.R;
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class LoginFragment extends Fragment {
+
+    @BindView(R.id.login_button)
+    Button loginButton;
 
     public LoginFragment() {
     }
@@ -18,6 +24,11 @@ public class LoginFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
         return view;
+    }
+
+    @OnClick(R.id.login_button)
+    public void doLogin() {
+        // TODO: login stuff
     }
 
 }
